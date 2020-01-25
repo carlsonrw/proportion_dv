bb4ca5028e508c8a16c8ee8a7fa875705f3121a1
Ryan <ryan.carlson@yale.edu>
2020-01-24 19:17:53 -0500

    new look

--- jspsych-6.1.0/plugins/jspsych-reconstruction.js
+++ jspsych-6.1.0/plugins/jspsych-reconstruction.js
@@ -51,7 +51,7 @@ jsPsych.plugins['reconstruction'] = (function() {
       button_label: {
         type: jsPsych.plugins.parameterType.STRING,
         pretty_name: 'Button label',
-        default:  'Continue',
+        default:  'Confirm response',
         description: 'The text that appears on the button to finish the trial.'
       }
     }
